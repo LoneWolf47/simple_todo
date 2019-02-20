e148c244cef03810f3d855a8d8b990d7da2fd7ec
LoneWolf47 <vikkyganesh47@gmail.com>
2019-02-20 14:19:59 +0530

    Initial Commit

---
+++ Server.js
@@ -0,0 +1,25 @@
+//Required Modules
+
+const express =require("express");
+const bodyParser = require('body-parser');
+const mongoose = require("mongoose");
+const rout =require("./route.js");
+
+const app = express();
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: false}));
+
+//MongoDB Connection 
+
+const dburl="mongodb://localhost:27017/tasks";
+mongoose.connect(dburl,{useNewUrlParser: true});
+mongoose.connection.on('error', function (err){console.log("Mongoose Connection Error");console.log(err);});
+
+app.use('/',rout);
+
+
+//Server 
+let port=8000;
+app.listen(port, () => {
+    console.log("Server is listening on port 8000");
+});
